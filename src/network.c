77d31e2dbbc5bcd73b52c75de322d699a14dc7c8
lockpicker1 <k4r4mp0l4@hotmail.com>
2015-12-29 16:50:14 +0200

    Correcting errors

--- src/network.c
+++ src/network.c
@@ -29,9 +29,8 @@ Public Function Declarations
 
 
 /*Network creation*/
-/*Returns address of created neural_net item*/
 /*NO ERROR CHECKING*/
-/*ONLY CHECKS FOR MEMOR ERRORS*/
+/*ONLY CHECKS FOR MEMORY ERRORS*/
 int create_network               (int               num_of_inputs,
                                   int               num_of_layers,
                                   int               *neurons_per_layer,//make this argument optional
@@ -40,6 +39,7 @@ int create_network               (int               num_of_inputs,
     struct neural_net *network;
     neuron *neuron_table_address;
     int neuron_counter,sum_of_neurons=0;
+    //Memory allocation of network
     network = malloc(sizeof(struct neural_net);
     if (network == nullptr) {//Check if memory was available to allocate
         printf("\n---NOT ENOUGH MEMORY FOR NETWORK CREATION---\n");
@@ -48,9 +48,11 @@ int create_network               (int               num_of_inputs,
     network->num_of_inputs      = num_of_inputs;
     network->num_of_layers      = num_of_layers;
     network->neurons_per_layer  = neurons_per_layer;
+    network->input_vector       = nullptr;
     for (neuron_counter = 0; neuron_counter < num_of_layers; neuron_counter++) {
         sum_of_neurons += *neurons_per_layer[neuron_counter];//Calculate the sum of neurons to allocate the required memory
     }
+    sum_of_neurons              += num_of_inputs;
     neuron_table_address        = malloc(sum_of_neurons*sizeof(neuron));
     if (neuron_table_address == nullptr) {//Check if memory was available to allocate
         printf("\n---NOT ENOUGH MEMORY FOR NETWORK CREATION---\n");
@@ -59,5 +61,19 @@ int create_network               (int               num_of_inputs,
     }
     network->neuron_table       = neuron_table_address;
     *network_addr = network;
-    return _CREATION_CORRECT;
+    //Network Setup
+    for (neuron_counter = num_of_inputs; neuron_counter < sum_of_neurons; neuron_counter++) {
+        neuron_init(&(network->neuron_table[neuron_counter]));
+    }
+    return _RETURN_SUCCESS;
+}
+
+
+void change_input          (double                  *input_vector,
+                            struct neural_net       *network_addr){
+    network_addr->input_vector = input_vector;
+}
+
+void network_activate      (struct neural_net       *network) {
+
 }
--- src/network.h
+++ src/network.h
@@ -16,7 +16,7 @@ This file is part of NeuroticNetwork.
     You should have received a copy of the GNU General Public License
     along with NeuroticNetwork.  If not, see <http://www.gnu.org/licenses/>.
 */
-#define _CREATION_CORRECT 1
+#define _RETURN_SUCCESS 1
 #define _CREATION_MEMORY_ERROR 2
 #include "neuron.h"
 #ifndef _network_h
@@ -40,7 +40,8 @@ This file is part of NeuroticNetwork.
 struct neural_net{                                 //Network Struct
         int                 num_of_inputs;             //Number of inputs of structure
         int                 num_of_layers;             //Number of layers in the neural network
-        int                 *neurons_per_layer         //Table of neurons per layer
+        int                 sum_of_neurons;            //Sum of neurons in network (Inputs are treates as neurons)
+        int                 *neurons_per_layer;        //Table of neurons per layer
         neuron              *neuron_table;             //Table of network interconnects (1 dimensional)
 };
 
@@ -53,10 +54,15 @@ struct neural_net{                                 //Network Struct
 /*Returns address of neural_net struct*/
 /*TO-DO: MAKE THIS VARADIC*/
 /*NO ERROR CHECKING*/
-int create_network         (int                 num_of_inputs,
-                            int                 num_of_layers,
-                            int                 *neurons_per_layer,//make this argument optional
-                            struct neural_net   **network_addr);
+int create_network         (int                     num_of_inputs,
+                            int                     num_of_layers,
+                            int                     *neurons_per_layer,//make this argument optional
+                            struct neural_net       **network_addr);
+
+/*Change the input vector of the network*/
+/*NO ERROR CHECKING*/
+void change_input          (double                  *input_vector,
+                            struct neural_net       *network_addr);
 
 /* Calculate the output of network */
 /* Calculation from the input layers to the output serially */
@@ -66,20 +72,20 @@ void network_activate      (struct neural_net       *network);
 /* Add the values of the deltaw array to the weight array of a single neuron */
 /* NO ERROR CHECKING */
 void network_delta         (struct neural_net       *network,
-                            int              layer_pointer,
-                            int              neuron_pointer,
-                            double           *deltaw);
+                            int                     layer_pointer,
+                            int                     neuron_pointer,
+                            double                  *deltaw);
 
 /* Set the weights of a neuron in the network equal to the deltaw array */
 /* NO ERROR CHECKING */
 void network_setw          (struct neural_net       *network,
-                            int              layer_pointer,
-                            int              neuron_pointer,
-                            double           *deltaw);
+                            int                     layer_pointer,
+                            int                     neuron_pointer,
+                            double                  *deltaw);
 
 /* Delete the network */
 /* NO ERROR CHECKING */
-void network_free          (struct neural_net       *network);
+void network_delete          (struct neural_net       *network);
 
 
 #endif
